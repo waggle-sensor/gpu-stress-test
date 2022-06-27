ccffaea4d5e693b4bad876cb8bc42db1a37d4f4e
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-27 15:08:27 -0500

    SAGE-1394: auto-linting

--- stress.py
+++ stress.py
@@ -1,18 +1,19 @@
-import torch
-import time
 import argparse
-import sys
 import os
+import sys
+import time
+
+import torch
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--Minutes", help = "Number of minutes to run the stress test")
+parser.add_argument("-m", "--Minutes", help="Number of minutes to run the stress test")
 args = parser.parse_args()
 
 num_minutes = float(args.Minutes)
 
-x = torch.linspace(0, 4, 16*1024**2).cuda()
+x = torch.linspace(0, 4, 16 * 1024 ** 2).cuda()
 
-timeout = time.time() + 60*num_minutes
+timeout = time.time() + 60 * num_minutes
 
 while True:
     x = x * (1.0 - x)
