591621264223d54c1631981c3d0324cd35012f02
Joe Swantek <joseph.swantek@northwestern.edu>
2022-08-25 13:11:39 -0500

    switch loop runtime to be in seconds

--- stress.py
+++ stress.py
@@ -7,15 +7,18 @@ import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-m", "--Minutes", nargs="?", const=5, type=int, help="Number of minutes to run the stress test"
+    "-r",
+    "--runtime",
+    nargs="?",
+    default=300,
+    type=int,
+    help="Number of seconds to run the stress test",
 )
 args = parser.parse_args()
 
-num_minutes = float(args.Minutes)
-
 x = torch.linspace(0, 4, 16 * 1024 ** 2).cuda()
 
-timeout = time.time() + 60 * num_minutes
+timeout = time.time() + args.runtime
 
 while True:
     x = x * (1.0 - x)
